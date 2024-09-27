fc057a0d8a4e6b1d7a4633a743d6e4a30cf691ed
Henni <henni.wie@gmail.com>
2024-09-27 13:13:22 +0200

    building server tutorial

--- incl/Webserver.hpp
+++ incl/Webserver.hpp
@@ -18,13 +18,41 @@
 // # include "IMessage.hpp"
 // # include "URI.hpp"
 
-class Webserver
+namespace http
 {
-	public:
-		Webserver();
-		~Webserver();
+	class Webserver
+	{
+		private:
+			std::string 	m_ip_address;
+			std::string 	m_serverMessage;
+			int				m_socket;
+			int				m_port;
+			int				m_new_socket;
+			long			m_incomingMessage;
+			unsigned int	m_socketAddress_len;
+			struct sockaddr	m_socketAddress;
 
-	private:
+			int				startServer();
+			void			closeServer();
+
+		public:
+			Webserver(std::string ip_address, int port);
+			~Webserver();
+
+	};
+} // namespace http
+
+struct	sockaddr {
+	short			sin_family; //for AF_Inet
+	unsigned short	sin_port; // htons(8080)
+	struct in_addr	sin_addr; // set to 0.0.0.0 as default IP
+	char			sin_zero[8]; //buffer used to adjust the size of the sockaddr_in
 };
+
+struct in_addr {
+	unsigned long	s_addr;
+};
+
+//https://osasazamegbe.medium.com/showing-building-an-http-server-from-scratch-in-c-2da7c0db6cb7
 
 #endif
