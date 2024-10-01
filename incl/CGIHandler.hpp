e87ef9e8143e3246fb40fac77bf681e742338751
Henni <henni.wie@gmail.com>
2024-10-01 11:12:20 +0200

    cgi start

---
+++ incl/CGIHandler.hpp
@@ -0,0 +1,18 @@
+
+#ifndef CGIHANDLER_HPP_
+#define CGIHANDLER_HPP_
+
+// Class declaration
+class CGIHandler {
+ public:
+  CGIHandler();
+  CGIHandler(const CGIHandler &other);
+  CGIHandler &operator=(const CGIHandler &rhs);
+  ~CGIHandler();
+
+ private:
+  
+};
+
+#endif // CGIHANDLER_HPP_
+
