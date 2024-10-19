e8a6536eb00e08ac122efe217d749c69713b1444
Vincent <v.ketteniss@outlook.com>
2024-10-19 18:12:44 +0200

    updated cgi execution: better logging

--- src/CGIExecutor.cpp
+++ src/CGIExecutor.cpp
@@ -10,7 +10,7 @@ CGIExecutor::~CGIExecutor()
 { return; }
 
 
-bool CGIExecutor::executeCGI(const Request & request, Response & response)
+bool CGIExecutor::executeCGI(Request & request, Response & response)
 {
 	char **env = createCGIEnvironment(request);
 	const char * path = request.getUri().c_str();
@@ -47,34 +47,37 @@ bool CGIExecutor::executeCGI(const Request & request, Response & response)
         close(pipe_fd[1]);
         int status;
         waitpid(pid, &status, 0);
-        if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+
+		std::string output;
+		char buffer[128];
+		ssize_t bytes_read;
+		while ((bytes_read = read(pipe_fd[0], buffer, sizeof(buffer) - 1)) > 0)
 		{
-			std::string output;
-			char buffer[128];
-			ssize_t bytes_read;
-			while ((bytes_read = read(pipe_fd[0], buffer, sizeof(buffer) - 1)) > 0)
-			{
-				buffer[bytes_read] = '\0';
-				output += buffer;         
-			}
-			close(pipe_fd[0]);
-			response.setBody(output);
-			return true;
-        }
+			buffer[bytes_read] = '\0';
+			output += buffer;         
+		}
+		close(pipe_fd[0]);
+		response.setBody(output);
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+		{
+            std::cerr << "CGI failed with error code " << status << std::endl;
+            std::cerr << "Output:  " << status << std::endl;
+			return false;
+		}
     }
-	return false;
+	return true;
 }
 
-char** CGIExecutor::createCGIEnvironment(const Request& request)
+char** CGIExecutor::createCGIEnvironment(Request& request)
 {
     std::map<std::string, std::string> env_map;
 
-    env_map["REQUEST_METHOD"] = request.getMethod();
+    env_map["REQUEST_METHOD"] = request.getMethod().substr(0);
     env_map["SCRIPT_NAME"] = request.getUri();
-    env_map["QUERY_STRING"] = request.getQueryString();
+    env_map["QUERY_STRING"] = request.buildQueryString();
     env_map["SERVER_PROTOCOL"] = request.getVersion();
-    env_map["CONTENT_TYPE"] = "text/html"; //request.getHeader("Content-Type");
-    env_map["CONTENT_LENGTH"] = "100"; //request.getHeader("Content-Length");
+    env_map["CONTENT_TYPE"] = request.getHeaderOrDefault("Content-Type", "");
+    env_map["CONTENT_LENGTH"] = request.getHeaderOrDefault("Content-Length", "0");
 
     char** env = new char*[env_map.size() + 1];
     int i = 0;
@@ -88,4 +91,5 @@ char** CGIExecutor::createCGIEnvironment(const Request& request)
     env[i] = NULL;
     return env;
 }
+		
 
